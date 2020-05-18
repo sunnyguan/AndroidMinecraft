2e1cdb6e0e3e8229217a6033bc4ecbddeb16823d
sunnyguan <sunny.peng.guan@gmail.com>
2020-05-18 09:37:39 -0700

    Added setup script

---
+++ minecraftSetup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+termux-wake-lock
+./start-ubuntu.sh
+apt install software-properties-common
+add-apt-repository ppa:openjdk-r/ppa
+apt-get update
+apt-get install openjdk-8-jre nano zip unzip
+mkdir mc
+cd mc
+wget -O minecraft_server.jar https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
+echo "eula=true" > eula.txt
+cd ..
+wget -O ngrok.zip https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip
+unzip ngrok.zip
+printf "cd mc\njava -Xmx1024MB -jar minecraft_server.jar nogui" > m
+printf "cd ngrok\n./ngrok tcp 25565" > n
