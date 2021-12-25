576e57bba10526c7ad229bd23be11f5187321414
Sunny Guan <sunny.peng.guan@gmail.com>
2021-12-25 14:57:05 -0800

    simple

--- minecraftSetup.sh
+++ minecraftSetup.sh
@@ -1,16 +1,86 @@
 #!/bin/bash
+
+##### UBUNTU INSTALLATION #####
+
+echo "STATUS: Setting up Ubuntu"
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  echo "WARN: first try failed, trying again"
+  pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+fi
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  echo "ERROR: Could not install ubuntu correctly, aborting."
+  exit $retVal
+fi
+
+echo "STATUS: Starting Ubuntu"
+./start-ubuntu.sh
+
+##### USER CONFIGURATIONS #####
+
+# set to true if you want to use forge, update FORGE_SERVER below to the correct version if necessary
+# leave as false if using vanilla, update VANILLA_SERVER below if necessary
+USE_FORGE=true
+
+# set to false if you have your own port-forwarding setup
+# leave as true to forward local ip to online through ngrok so other people can join
+USE_NGROK=true
+
+# forge server URL (1.18.1), update as necessary
+FORGE_SERVER="https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.1-39.0.8/forge-1.18.1-39.0.8-installer.jar"
+
+# current version (1.18.1), update as necessary
+VANILLA_SERVER="https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar"
+
+# if you see an error about class version in the future, 
+# check https://stackoverflow.com/questions/9170832/list-of-java-class-file-format-major-version-numbers
+# and update this to the appropriate version
+JDK_VERSION=17
+
+# don't need to edit this
+EXEC_SERVER_NAME="minecraft_server.jar"
+
+##### MINECRAFT/NGROK INSTALLATION #####
+
+# install openjdk
+echo "STATUS: installing openjdk-${JDK_VERSION}" 
 apt-get update
 apt install software-properties-common -y
 add-apt-repository ppa:openjdk-r/ppa -y
-apt-get install openjdk-17-jre zip unzip -y
+apt-get install openjdk-$JDK_VERSION-jre-headless zip unzip -y
+
+# minecraft server download and setup
+echo "STATUS: setting up Minecraft Server"
 mkdir mc
 cd mc
-wget -O minecraft_server.jar https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar
 echo "eula=true" > eula.txt
-cd ..
-wget -O ngrok.zip https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip
-unzip ngrok.zip
-printf "cd mc\njava -Xmx1G -jar minecraft_server.jar nogui" > m
-printf "cd ngrok\n./ngrok tcp 25565" > n
-chmod +x m
-chmod +x n
+if [ "$USE_FORGE" = true ] ; then
+  wget $FORGE_SERVER
+  installer_jar=$(echo $FORGE_SERVER | rev | cut -d '/' -f 1 | rev)
+  exec_jar=$(echo $installer_jar | sed -e 's/-installer//g')
+  java -jar $installer_jar --installServer
+  mv $exec_jar $EXEC_SERVER_NAME
+  rm $installer_jar
+  echo "cd mc && ./run.sh" > m
+else
+  wget -O $EXEC_SERVER_NAME $VANILLA_SERVER
+  echo "cd mc && java -Xmx1G -jar ${EXEC_SERVER_NAME} nogui" > m
+  chmod +x m
+fi
+  
+
+# ngrok download and setup
+if [ "$USE_NGROK" = true ] ; then
+  echo "STATUS: setting up ngrok"
+  cd ..
+  wget -O ngrok.tgz https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.tgz && tar -xvzf ngrok.tgz
+  echo "./ngrok tcp 25565" > n
+  chmod +x n
+fi
+
+
+echo "STATUS: installation complete! Run ./m here to start minecraft server, open a new session by swiping on the left, and run ./n there to start ngrok"
