a66242a0948a12b9da5f7719fea79e51035b22b0
Sunny Guan <sunny.peng.guan@gmail.com>
2021-12-25 12:00:10 -0800

    bump jre, bump server to 1.18

--- minecraftSetup.sh
+++ minecraftSetup.sh
@@ -2,10 +2,10 @@
 apt-get update
 apt install software-properties-common -y
 add-apt-repository ppa:openjdk-r/ppa -y
-apt-get install openjdk-8-jre zip unzip -y
+apt-get install openjdk-17-jre zip unzip -y
 mkdir mc
 cd mc
-wget -O minecraft_server.jar https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
+wget -O minecraft_server.jar https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar
 echo "eula=true" > eula.txt
 cd ..
 wget -O ngrok.zip https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip
